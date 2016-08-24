e51c88e8c9b37046fa741d8097c91b017e853a0e
Alex R Chies <alexrchies@gmail.com>
2016-08-24 20:40:27 +0200

    fix(webpack): fix compilation issues in windows (#8)

--- config-web/webpack.dev.js
+++ config-web/webpack.dev.js
@@ -2,21 +2,23 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+var Dashboard = require('webpack-dashboard');
 var DashboardPlugin = require('webpack-dashboard/plugin');
+var dashboard = new Dashboard();
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'source-map',
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://0.0.0.0:8080/',
+    publicPath: 'http://localhost:8080/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
 
   plugins: [
     new ExtractTextPlugin('[name].css'),
-    new DashboardPlugin()
+    new DashboardPlugin(dashboard.setData)
   ],
 
   tslint: {

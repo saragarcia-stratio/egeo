e40cb064f8bee5f46c34884ba720c1aabacba822
luismls86 <luismls86@gmail.com>
2016-09-07 17:13:14 +0200

    TranslatePipe removed from everywhere (#23)

--- components/st-info-box/st-info-box.component.ts
+++ components/st-info-box/st-info-box.component.ts
@@ -1,18 +1,16 @@
-import { Component, Input, Injectable} from '@angular/core';
-import { TranslatePipe } from 'ng2-translate/ng2-translate';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'st-info-box',
-  pipes: [TranslatePipe],
   template: require('./st-info-box.component.html'),
   styles: [require('./st-info-box.component.scss')]
 })
-@Injectable()
+
 export class StInfoBoxComponent {
   @Input() icon: string;
   @Input() title: string;
 
   constructor() {
   }
-  
+
 }

3b53743606c62d71d32388e6ec71377d03ad5683
Pedro Peñalver Yusta <ppenalver@stratio.com>
2016-09-22 08:45:15 +0200

    EGE-64 export all necesary classes for modals (#40)

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,7 @@ export { STVALIDATIONS } from './st-validations';
 export { StInputError } from './st-input';
 export { StModificableListElement, StListModifierObject } from './st-modificable-list';
 export { StFooterLink } from './st-footer';
-export { BUTTON_TYPES, Buttons, StModalService, MessageModal } from './st-modal';
+export { BUTTON_TYPES, Buttons, StModalService, MessageModal, ModalConfig, ModalTitle } from './st-modal';
 export { GosecListComponent } from './st-table';
 export { EgeoModule } from './egeo.module';
 export * from './utils';

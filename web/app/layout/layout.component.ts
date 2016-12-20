bf4fb926e412b504ccd62155d1fa331bd088b8de
Alex R Chies <alexrchies@gmail.com>
2016-12-20 10:39:20 +0100

    fix(dropdown): fix some graphic issues (#139)

--- web/app/layout/layout.component.ts
+++ web/app/layout/layout.component.ts
@@ -8,17 +8,9 @@ import { Router } from '@angular/router';
 })
 
 export class LayoutComponent {
-   @Input() isBrandingOpened: boolean;
-   @Input() isStyleOpened: boolean;
-   @Input() isCatalogOpened: boolean;
-   @Input() isGettingStartedOpened: boolean;
    @Input() currentUrl: string;
 
    constructor(private router: Router) {
-      this.isBrandingOpened = false;
-      this.isStyleOpened = false;
-      this.isCatalogOpened = false;
-      this.isGettingStartedOpened = false;
 
       router.events.subscribe((val) => {
          this.currentUrl = val.url;

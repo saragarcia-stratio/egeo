fd829fd94b38582f3b69011ed0b21197cc2994f6
Alex R Chies <alexrchies@gmail.com>
2016-12-21 18:00:11 +0100

    EGE-171: Fix Button Issues (#145)
    * fix(button): set the default value of inputType to button
    
    * documentation(button): fix errors in param names
    
    * documentation(button): fix bad default value of inputType in params

--- web/app/+examples/button/button.component.ts
+++ web/app/+examples/button/button.component.ts
@@ -15,15 +15,14 @@ export class ButtonComponent {
       haveModel: false,
       apiSection: {
          inputs: [
-            { paramName: 'inputType', type: TYPES.STR, required: false, details: 'The button type can have one of these possible values: submit / reset / button. The submit and reset ones are used to be recognized as the HTML forms predefined options. Default type is submit.' },
+            { paramName: 'inputType', type: TYPES.STR, required: false, details: 'The button type can have one of these possible values: submit / reset / button. The submit and reset ones are used to be recognized as the HTML forms predefined options. Default type is button.' },
             { paramName: 'isDisabled', type: TYPES.BOOL, required: false, details: 'The button type can have one of these possible values: submit / reset / button. The submit and reset ones are used to be recognized as the HTML forms predefined options. Default type is submit.' },
-            { paramName: 'click', type: TYPES.FUNC, required: true, details: 'Callback function called on click on the Buttons.' },
-            { paramName: 'value', type: TYPES.STR, required: true, details: 'Text of the button. Default type is "Click Me".' },
             { paramName: 'qaTag', type: TYPES.STR, required: true, details: 'Id value for qa test.' },
             { paramName: 'leftIcon', type: TYPES.STR, required: false, details: 'An icon of the Stratio Fonticon family of icons that should appear at left of the text.' },
             { paramName: 'onClick', type: TYPES.FUNC, required: true, details: 'Callback function called on click on the button.' },
             { paramName: 'rightIcon', type: TYPES.STR, required: false, details: 'An icon of the Stratio Fonticon family of icons that should appear at right of the text.' },
             { paramName: 'subtypeClass', type: TYPES.STR, required: false, details: 'Subtype of button. Only for graphic purposes. It defines graphical variations of the type and always use the same color variations related to the name of subtype. Subtypes are subtype1, subtype2, subtype3... Not all buttons have all subtypes. Default is the subtype1 value.' },
+            { paramName: 'text', type: TYPES.STR, required: true, details: 'Text of the button. Default type is "Click Me".' },
             { paramName: 'themeClass', type: TYPES.STR, required: false, details: 'Theme applied to the button. Only for graphic purposes. Themes are created related to the context the button is placed in(i.e. the same button in different backgrounds). There is not default value.' },
             { paramName: 'typeClass', type: TYPES.STR, required: true, details: 'Type of button. Only for graphic purposes. It defines different types or buttons like classical rect buttons, the rounded ones or ones without background, similar to links. Types are btnMain, btnLink and btnTool. btnMain is the default value.' },
          ],

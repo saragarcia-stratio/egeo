c8742e8d9bf990da3fc7e2fcfe652b6e902d7b73
Pedro José Peña <pjpena@stratio.com>
2017-01-30 14:16:08 +0100

    Bugfix Test

--- components/st-dropdown/st-dropdown.component.spec.ts
+++ components/st-dropdown/st-dropdown.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import StDropdownComponent from './st-dropdown.component';
+import { StDropDownMenuItem } from '../st-dropdown-menu/st-dropdown-menu.interface';
+import StDropdownMenuComponent from '../st-dropdown-menu/st-dropdown-menu.component';
+import StDropDownMenuItemComponent from '../st-dropdown-menu/st-dropdown-menu-item.component';
+
+let items: Array<StDropDownMenuItem> = [
+   {
+      label: 'example 1',
+      value: 1
+   },
+   {
+      label: 'example 2',
+      value: 2
+   }
+];
 
 describe('StDropdownComponent', () => {
 
+   let component: StDropdownComponent;
+   let fixture: ComponentFixture<StDropdownComponent>;
+
+
    beforeEach(() => {
       TestBed.configureTestingModule({
-         declarations: [ StDropdownComponent ]
+         declarations: [ StDropdownComponent, StDropdownMenuComponent, StDropDownMenuItemComponent ]
       });
-   });
 
-   let component: StDropdownComponent;
+      fixture = TestBed.createComponent(StDropdownComponent);
+      component = fixture.componentInstance;
 
-   beforeEach(() => {
-      component = new StDropdownComponent();
    });
 
    it('should be a dropdown inactive by default', () => {
+      component.button = 'Example';
+      component.items = items;
+      fixture.detectChanges();
       expect(component.active).toBeFalsy();
    });
 
    it('should be a dropdown active', () => {
+      component.button = 'Example';
+      component.items = items;
       component.active = true;
-      expect(component.active).toBeTruthy();
+      fixture.detectChanges();
+      expect(fixture.componentInstance.active).toBeTruthy();
    });
 
 });

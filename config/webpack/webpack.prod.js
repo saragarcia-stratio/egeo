dc5ddefaf1f1a05ed9f60b2a777190b31ef1cb8d
pjpenast <pjpena@stratio.com>
2017-03-02 14:57:21 +0100

    Feature/gzip (#46)
    * Update yarn.lock
    
    * Update yarn.lock
    
    * Add Gzip
    
    * Update package.json

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -13,6 +13,7 @@ const NormalModuleReplacementPlugin = require('webpack/lib/NormalModuleReplaceme
 const ProvidePlugin = require('webpack/lib/ProvidePlugin');
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 const HOST = process.env.HOST || 'localhost';
@@ -23,106 +24,113 @@ module.exports = function (env) {
   return webpackMerge(commonConfig({
     env: ENV
   }), {
-    devtool: 'source-map',
-    output: {
-      path: helpers.root('target', 'site', 'web'),
-      filename: '[name].[chunkhash].bundle.js',
-      sourceMapFilename: '[name].[chunkhash].bundle.map',
-      chunkFilename: '[id].[chunkhash].chunk.js'
-    },
+      devtool: 'source-map',
+      output: {
+        path: helpers.root('target', 'site', 'web'),
+        filename: '[name].[chunkhash].bundle.js',
+        sourceMapFilename: '[name].[chunkhash].bundle.map',
+        chunkFilename: '[id].[chunkhash].chunk.js'
+      },
 
-    module: {
+      module: {
 
-      rules: [
+        rules: [
 
-              {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-          include: [helpers.root('src', 'styles')]
-        },
-        {
-          test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-          include: [helpers.root('src', 'styles')]
-        },
-
-
-      ]
-
-    },
-    plugins: [
-      new OptimizeJsPlugin({
-        sourceMap: false
-      }),
-      new ExtractTextPlugin('[name].[contenthash].css'),
-      new DefinePlugin({
-        'ENV': JSON.stringify(ENV),
-        'HMR': HMR,
-        'process.env': {
-          'ENV': JSON.stringify(ENV),
-          'NODE_ENV': JSON.stringify(ENV),
-          'HMR': HMR,
-        }
-      }),
-      new UglifyJsPlugin({
-        beautify: false, //prod
-        output: {
-          comments: false
-        }, //prod
-        mangle: {
-          screw_ie8: true
-        }, //prod
-        compress: {
-          screw_ie8: true,
-          warnings: false,
-          conditionals: true,
-          unused: true,
-          comparisons: true,
-          sequences: true,
-          dead_code: true,
-          evaluate: true,
-          if_return: true,
-          join_vars: true,
-          negate_iife: false
-        },
-      }),
-      new NormalModuleReplacementPlugin(
-        /angular2-hmr/,
-        helpers.root('config/empty.js')
-      ),
-
-      new NormalModuleReplacementPlugin(
-        /zone\.js(\\|\/)dist(\\|\/)long-stack-trace-zone/,
-        helpers.root('config/empty.js')
-      ),
-
-      new LoaderOptionsPlugin({
-        minimize: true,
-        debug: false,
-        options: {
-          htmlLoader: {
-            minimize: true,
-            removeAttributeQuotes: false,
-            caseSensitive: true,
-            customAttrSurround: [
-              [/#/, /(?:)/],
-              [/\*/, /(?:)/],
-              [/\[?\(?/, /(?:)/]
-            ],
-            customAttrAssign: [/\)?\]?=/]
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+            include: [helpers.root('src', 'styles')]
+          },
+          {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            include: [helpers.root('src', 'styles')]
           },
 
-        }
-      }),
-    ],
-    node: {
-      global: true,
-      crypto: 'empty',
-      process: false,
-      module: false,
-      clearImmediate: false,
-      setImmediate: false
-    }
 
-  });
+        ]
+
+      },
+      plugins: [
+        new OptimizeJsPlugin({
+          sourceMap: false
+        }),
+        new ExtractTextPlugin('[name].[contenthash].css'),
+        new DefinePlugin({
+          'ENV': JSON.stringify(ENV),
+          'HMR': HMR,
+          'process.env': {
+            'ENV': JSON.stringify(ENV),
+            'NODE_ENV': JSON.stringify(ENV),
+            'HMR': HMR,
+          }
+        }),
+        new CompressionPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.js$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+        new UglifyJsPlugin({
+          beautify: false, //prod
+          output: {
+            comments: false
+          }, //prod
+          mangle: {
+            screw_ie8: true
+          }, //prod
+          compress: {
+            screw_ie8: true,
+            warnings: false,
+            conditionals: true,
+            unused: true,
+            comparisons: true,
+            sequences: true,
+            dead_code: true,
+            evaluate: true,
+            if_return: true,
+            join_vars: true,
+            negate_iife: false
+          },
+        }),
+        new NormalModuleReplacementPlugin(
+          /angular2-hmr/,
+          helpers.root('config/empty.js')
+        ),
+
+        new NormalModuleReplacementPlugin(
+          /zone\.js(\\|\/)dist(\\|\/)long-stack-trace-zone/,
+          helpers.root('config/empty.js')
+        ),
+
+        new LoaderOptionsPlugin({
+          minimize: true,
+          debug: false,
+          options: {
+            htmlLoader: {
+              minimize: true,
+              removeAttributeQuotes: false,
+              caseSensitive: true,
+              customAttrSurround: [
+                [/#/, /(?:)/],
+                [/\*/, /(?:)/],
+                [/\[?\(?/, /(?:)/]
+              ],
+              customAttrAssign: [/\)?\]?=/]
+            },
+
+          }
+        }),
+      ],
+      node: {
+        global: true,
+        crypto: 'empty',
+        process: false,
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+      }
+
+    });
 }

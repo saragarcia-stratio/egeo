47c326441f0ffacfc8bf3bed305eb87481aaa4ce
Pedro Peñalver Yusta <ppenalver@stratio.com>
2017-03-02 09:36:25 +0100

    add Require decorator (#44)
    * add Require decorator
    
    * remove comments

---
+++ components/utils/egeo-utils.ts
@@ -0,0 +1,22 @@
+export class EgeoUtils {
+
+   static isDefined(value: any): boolean {
+      return value !== undefined && value !== null;
+   }
+
+   static validateInputs(scope: any, inputs: Array<string>, component: string): void {
+      inputs.forEach(input => {
+         if (!this.isDefined(scope[input])) {
+            throw new Error(`${this.toDash(component)}: field ${input} is a required field`);
+         }
+      });
+   }
+
+   static toDash(value: string): string {
+      try {
+         return value.replace(/([A-Z])/g, $1 => '-' + $1.toLowerCase()).substring(1);
+      } catch (err) {
+         return value;
+      }
+   };
+}

9a59fd1a9a11b2fe7e6fbedb5efa2ce51ab9a9ea
Pedro Peñalver Yusta <ppenalver@stratio.com>
2017-05-03 14:05:50 +0200

    fix(utils): export with full path (#219)

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,5 @@
 export { StRegEx } from './st-regex';
 export * from './event-window-manager';
-export * from './egeo-resolver';
+export * from './egeo-resolver/index';
 export { EgeoUtils } from './egeo-utils';
 export * from './unique-dispatcher';
